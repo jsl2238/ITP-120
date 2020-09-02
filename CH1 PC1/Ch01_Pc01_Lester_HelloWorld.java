cc6cfc35c3f79c8896b616286cc894ac9f4aa561
Jonathan <jsl2238@email.vccs.edu>
2020-09-01 21:07:09 -0400

    Ch01_Pc_01

---
+++ CH1 PC1/Ch01_Pc01_Lester_HelloWorld.java
@@ -0,0 +1,16 @@
+//********************************************************************
+//*   @Jonathan Lester
+//*   09/06/2020
+//*   Prints "Hello World!" and "My name is Jonathan." with a space in between.
+//********************************************************************
+
+public class Ch01_Pc01_Lester_HelloWorld //don't forget class names start with a cap
+{
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world!\n"); // Print out "Hello world!" and then creates a new line after.
+        System.out.println("My name is Jonathan."); // Print out My name is Jonathan!
+    } // end main()
+
+} // end HelloWorld
