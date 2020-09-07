7f7a84ae2c0ef4a093e2b49e7cfc13ee9331653b
Jonathan <jsl2238@email.vccs.edu>
2020-09-07 13:30:28 -0400

    Created CH2_PC12

--- CH1 PC1/Ch01_Pc01_Lester_HelloWorld.java
+++ CH1 PC1/Ch01_Pc01_Lester_HelloWorld.java
@@ -11,6 +11,7 @@ public class Ch01_Pc01_Lester_HelloWorld //don't forget class names start with a
     {
         System.out.println("Hello world!\n"); // Print out "Hello world!" and then creates a new line after.
         System.out.println("My name is Jonathan."); // Print out My name is Jonathan! (this is a change!)
+        System.out.println(1/2);
     } // end main()
 
 } // end HelloWorld
--- README.md
+++
@@ -1,2 +0,0 @@
-# ITP-120
-This GitRepo is going to be used to keep all of my school projects aligned and up to date!
