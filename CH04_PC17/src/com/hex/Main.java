de5958313e76a6f11c8369db1799dc1ae00ff56c
Jonathan <jsl2238@email.vccs.edu>
2020-10-04 22:50:30 -0400

    Finished project Ch04_Pc17

--- CH04_PC17/src/com/hex/Main.java
+++ CH04_PC17/src/com/hex/Main.java
@@ -1,12 +1,41 @@
 package com.hex;
 
-import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        PrintWriter outputFile = new PrintWriter("test_File");
-        outputFile.println("this works!");
-        outputFile.close();
+    public static void main(String[] args)
+    {
+        //Housekeeping
+        Scanner input = new Scanner(System.in);
+        int num, userGuess = 0;
+        num = (int) (Math.random() * 100);
+
+        //Input
+
+        //Processing
+
+        while (userGuess != num)
+        {
+            System.out.printf("\n\nPlease pick a number between 0 - 100: ");
+            userGuess = input.nextInt();
+            if (userGuess >= 0 && userGuess <= 100)
+            {
+                if (userGuess < num)
+                    System.out.printf("Too low, try again.");
+                if (userGuess > num)
+                    System.out.printf("Too high, try again.");
+                if (userGuess == num)
+                    System.out.printf("You win!");
+            } else
+                {
+                    System.out.printf("You entered an invalid number, restart the program and try again!");
+                    break;
+                }
+        }
+
+        //Output
+
+
     }
 }
