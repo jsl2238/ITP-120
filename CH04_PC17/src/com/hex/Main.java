c23c82e648131cdac1741803b872f4f73f170a5d
Jonathan <jsl2238@email.vccs.edu>
2020-10-12 20:40:09 -0400

    Fixed project Ch04_Pc17

--- CH04_PC17/src/com/hex/Main.java
+++ CH04_PC17/src/com/hex/Main.java
@@ -2,40 +2,40 @@ package com.hex;
 
 import java.util.Scanner;
 
-public class Main {
-
+public class Main
+{
     public static void main(String[] args)
     {
         //Housekeeping
-        Scanner input = new Scanner(System.in);
-        int num, userGuess = 0;
-        num = (int) (Math.random() * 100);
+        Scanner input = new Scanner(System.in);                                           //Define input
+        final String PICKNUM, WINMSG, TOOLOWMSG, TOOHIGHMSG, INVALIDMSG;                  //Define all strings to be used
+        String question;
+        int randomNum, userGuess = -1;                                                    //Define all integers to use
 
-        //Input
+        randomNum = (int) (Math.random() * 101);                                          //Randomly generating a number
+        PICKNUM = "\n\nPlease pick a number between 0 - 100: ";                           //Ask user to pick a number
+        WINMSG = "\n\nYou win!";                                                          //Define the message for if user wins
+        TOOLOWMSG = "Too low, try again.";                                                //Define the message for if user's guess was too low
+        TOOHIGHMSG = "Too high, try again.";                                              //Define the message for if the user's guess was too high
+        INVALIDMSG = "Invalid number, try again!";                                        //Define the message for if the user's input is in valid
 
-        //Processing
-
-        while (userGuess != num)
+        while (userGuess != randomNum)                                                    //Start of while loop (As long as the user's guess does not equal the number
         {
-            System.out.printf("\n\nPlease pick a number between 0 - 100: ");
-            userGuess = input.nextInt();
-            if (userGuess >= 0 && userGuess <= 100)
+            System.out.printf(PICKNUM);                                                   //Ask the user to pick a number
+            userGuess = input.nextInt();                                                  //Get the user's input
+            if (userGuess >= 0 && userGuess <= 100)                                       //Test if user's guess is greater than or equal to 0 and that the user guess is less than or equal to 100
             {
-                if (userGuess < num)
-                    System.out.printf("Too low, try again.");
-                if (userGuess > num)
-                    System.out.printf("Too high, try again.");
-                if (userGuess == num)
-                    System.out.printf("You win!");
-            } else
-                {
-                    System.out.printf("You entered an invalid number, restart the program and try again!");
-                    break;
-                }
-        }
+                if (userGuess < randomNum)                                                //Test if the user guess is less than the number
+                    System.out.printf(TOOLOWMSG);                                         //If it is, then print out the tooLowMsg
+                if (userGuess > randomNum)                                                //Test if the user guess is higher than the number
+                    System.out.printf(TOOHIGHMSG);                                        //If it is, then print out the tooHighMsg
+                if (userGuess == randomNum)                                               //Test if the user guess the answer right
+                    System.out.printf(WINMSG);                                            //If it is, print out winMsg
+            } else                                                                        //If the user guess doesn't follow the previous criteria
+            {
+                System.out.printf(INVALIDMSG);                                            //Print out that the user has an invalid guess
+            } //if (userGuess >= 0 && userGuess <= 100) and else
+        } //while (userGuess != randomNum)
+    } //main()
+} //Class Ch04_Pc17_Lester()
 
-        //Output
-
-
-    }
-}
