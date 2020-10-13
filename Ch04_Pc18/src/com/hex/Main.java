70f26de4d3bf51e9516cce62ed241a15b5717f25
Jonathan <jsl2238@email.vccs.edu>
2020-10-12 21:00:01 -0400

    Added counter and fixed minor formatting issues

--- Ch04_Pc18/src/com/hex/Main.java
+++ Ch04_Pc18/src/com/hex/Main.java
@@ -9,11 +9,11 @@ public class Main
         //Housekeeping
         Scanner input = new Scanner(System.in);                                           //Define input
         final String PICKNUM, WINMSG, TOOLOWMSG, TOOHIGHMSG, INVALIDMSG;                  //Define all strings to be used
-        int randomNum, userGuess = -1;                                                    //Define all integers to use
+        int randomNum, counter = 0, userGuess = -1;                                       //Define all integers to use
 
         randomNum = (int) (Math.random() * 101);                                          //Randomly generating a number
         PICKNUM = "\n\nPlease pick a number between 0 - 100: ";                           //Ask user to pick a number
-        WINMSG = "\n\nYou win!";                                                          //Define the message for if user wins
+        WINMSG = "\n\nYou win! It took you %d guesses to get it right!";                  //Define the message for if user wins
         TOOLOWMSG = "Too low, try again.";                                                //Define the message for if user's guess was too low
         TOOHIGHMSG = "Too high, try again.";                                              //Define the message for if the user's guess was too high
         INVALIDMSG = "Invalid number, try again!";                                        //Define the message for if the user's input is in valid
@@ -24,12 +24,13 @@ public class Main
             userGuess = input.nextInt();                                                  //Get the user's input
             if (userGuess >= 0 && userGuess <= 100)                                       //Test if user's guess is greater than or equal to 0 and that the user guess is less than or equal to 100
             {
+                counter+=1;
                 if (userGuess < randomNum)                                                //Test if the user guess is less than the number
                     System.out.printf(TOOLOWMSG);                                         //If it is, then print out the tooLowMsg
                 if (userGuess > randomNum)                                                //Test if the user guess is higher than the number
                     System.out.printf(TOOHIGHMSG);                                        //If it is, then print out the tooHighMsg
                 if (userGuess == randomNum)                                               //Test if the user guess the answer right
-                    System.out.printf(WINMSG);                                            //If it is, print out winMsg
+                    System.out.printf(WINMSG, counter);                                            //If it is, print out winMsg
             } else                                                                        //If the user guess doesn't follow the previous criteria
             {
                 System.out.printf(INVALIDMSG);                                            //Print out that the user has an invalid guess
